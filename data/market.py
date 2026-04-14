703a695e4f4fdc548aeb392f8f1128321a9b8c1b
danielaguilar12032006-afk <danielaguilar12032006@gmail.com>
2026-04-14 15:08:41 -0400

    Update market.py

--- data/market.py
+++ data/market.py
@@ -1,8 +1,18 @@
-import requests
+import ccxt
 
-def get_price():
-    url = "https://api.kraken.com/0/public/Ticker?pair=XBTUSDT"
-    response = requests.get(url)
-    data = response.json()
+exchange = ccxt.kraken()
 
-    return float(data["result"]["XBTUSDT"]["c"][0])
+SYMBOLS = ["BTC/USDT", "ETH/USDT", "SOL/USDT"]
+
+def get_prices():
+    prices = {}
+
+    for symbol in SYMBOLS:
+        try:
+            ticker = exchange.fetch_ticker(symbol)
+            coin = symbol.split("/")[0]
+            prices[coin] = ticker["last"]
+        except Exception as e:
+            print(f"Error fetching {symbol}: {e}")
+
+    return prices
